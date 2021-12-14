ddf8c1fb742b5876ffd4e9448995d5e06413cd51
Emil <50626652+TheDovah@users.noreply.github.com>
2021-12-14 09:29:29 +0000

    Add files via upload

---
+++ main.php
@@ -0,0 +1,77 @@
+<?php
+
+include 'header.php';
+
+?>
+<head>
+    <link href="css/General.css" rel="stylesheet">
+    <link href="css/mainStylesheet.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="wrapper">
+
+        <div class="piece">
+            <h1>Forklife</h1>
+            <h3 class = "gamePro">Platformer</h3>
+            <p>
+                Forklife started as an idea in my head for a game about forklifts and cargo shipping. Then when I got the opportunity to choose a game project I chose to make Forklife a reality as my first ever self-made game. Forklife allowed me to engage and involve myself within Unity and actually learn how to make a game from scratch.
+
+                <br><br>
+
+                The assets I used are self made basic cubes and the textures are from textureheaven.
+
+                <br><br>
+
+
+                <div class="itchWidget">
+                    <iframe src="https://itch.io/embed/634678?bg_color=222222&amp;fg_color=eeeeee&amp;link_color=ff4e33&amp;border_color=363636" width="208" height="167" frameborder="0"><a href="https://emilsja.itch.io/forklife">ForkLife - Life of a forklift by EmilSja</a></iframe>
+                    <!--To download the game, go <a href="https://emilsja.itch.io/forklife" target="_blank">here</a>.-->
+                </div>
+            </p>
+        </div>
+
+        <div class = "picture">
+            <img class="forkSlides" src="images/ForklifeLevel1.png">
+            <img class="forkSlides" src="images/ForklifeLevel2.png">
+        </div>
+
+        <div class="piece1">
+            <h1>Deck of 2020</h1>
+            <h3 class = "gamePro">Card Game</h3>
+                <p>
+
+                    Deck of 2020 is a card game made for the annual GMTK Game Jam of 2020. Which I was in a team with one other person. I had responsibility of how the cards looked and their behaviour.
+
+                    <br><br>
+
+                    The assets in-game are self made. This game is also fully modular so the player can put in other cards they want to play with.
+
+                    <br><br>
+
+                </p>
+
+            <div class="itchWidget">
+                <iframe src="https://itch.io/embed/697622?border_width=3&amp;bg_color=222222&amp;fg_color=eeeeee&amp;link_color=ff4e33&amp;border_color=363636" width="212" height="171" frameborder="0"><a href="https://emilsja.itch.io/deck-of-2020">Deck of 2020 by EmilSja</a></iframe>
+            </div>
+        </div>
+
+        <div class = "picture2">
+            <img src="images/DeckOf2020.png"
+        </div>
+
+        <div class="references">
+            <h3>References</h3>
+            <p>For the font: <a href="https://fonts.google.com/specimen/Alfa+Slab+One?preview.size=50&preview.text=Forklife&preview.text_type=custom&sidebar.open&selection.family=Alfa+Slab+One" target="_blank">Google Fonts</a></p>
+            <p>Hamburger menu and animation is from w3school</p>
+        </div>
+
+        <div class="footer">&copy; 2020, Emil.</div>
+
+    </div>
+</body>
+
+<script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js" defer></script>
+<script src="index.js" ></script>
+
+</html>
