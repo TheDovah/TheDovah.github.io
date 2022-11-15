3dde792c4fdf9d71f7b012dedf65fbaa16295517
TheDovah <johasja@outlook.com>
2022-11-15 10:42:27 +0100

    Drop down menu on phone

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ function myFunction() {
   }
 } 
 
-/*
 function openSlideMenu()
 {
     document.getElementById('menu').style.width = '250px';
@@ -52,4 +51,3 @@ function closeSlideMenu()
     document.getElementById('menu').style.width = '0';
     document.getElementById('content').style.marginLeft = '0';
 }
-*/
